e25e277ba249e4206668d1a9bd58161184060b74
Your Name <you@example.com>
2024-05-16 16:22:51 +0200

    Light Mode Button Icons

--- src/components/LightMode.js
+++ src/components/LightMode.js
@@ -1,24 +1,27 @@
 import React, { useState, useEffect } from 'react';
-import '../styles/colors.css';
+import '../styles/colors.css';  // Richtiges Importieren der CSS-Datei
+import sunIcon from '../images/icons/light-mode-icon.svg'; 
+import moonIcon from '../images/icons/dark-mode-icon.svg'; 
 
 function LightMode() {
   const [theme, setTheme] = useState('dark');
 
-  // Setze das Theme-Attribut beim ersten Rendern und bei Änderungen des Themes
   useEffect(() => {
     document.documentElement.setAttribute('data-theme', theme);
   }, [theme]);
 
-  // Funktion zum Umschalten des Themes
   const toggleTheme = () => {
     setTheme((prevTheme) => (prevTheme === 'dark' ? 'light' : 'dark'));
   };
 
   return (
-    <div>
+    <div style={{  color: 'var(--text-color)', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
       <label className="switch">
         <input type="checkbox" onChange={toggleTheme} checked={theme === 'light'} />
-        <span className="slider"></span>
+        <span className="slider">
+          <img src={moonIcon} alt="Moon Icon" className="icon moon-icon" />
+          <img src={sunIcon} alt="Sun Icon" className="icon sun-icon" />
+        </span>
       </label>
     </div>
   );
