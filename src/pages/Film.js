7105ede81292689cdcc0b9bccab2b594bc4fd504
Your Name <you@example.com>
2024-05-20 20:55:20 +0200

    Film width fix

--- src/pages/Film.js
+++ src/pages/Film.js
@@ -2,11 +2,13 @@ import CustomVideoPlayer from "../components/CustomVideoPlayer"
 
 const Film = () => {
   return (
-    <div className="film-section">
-    <div className="interactive-video-container">
-        <h1>Film</h1>
-        <CustomVideoPlayer />
-    </div>
+    <div className="film-section-container">
+      <div className="film-section">
+        <div className="interactive-video-container">
+            <h1>Film</h1>
+            <CustomVideoPlayer />
+        </div>
+      </div>
     </div>
   )
 }
