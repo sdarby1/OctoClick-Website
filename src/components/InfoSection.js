e73e2f4abd6c19cfdad278704f14fda434fd696e
Your Name <you@example.com>
2024-05-20 20:16:38 +0200

    Nav Animation changes

--- src/components/InfoSection.js
+++ src/components/InfoSection.js
@@ -42,7 +42,7 @@ const InfoSection = () => {
           ref={ref2}
         >
           <h2>Headline Info</h2>
-          <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr...</p>
+          <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
         </motion.div>
       </div>
       <div className="info-section">
@@ -55,7 +55,7 @@ const InfoSection = () => {
           ref={ref3}
         >
           <h2>Headline Info</h2>
-          <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr...</p>
+          <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
         </motion.div>
         <motion.div
           className="info-image-container"
